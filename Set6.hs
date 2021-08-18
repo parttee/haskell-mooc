495ed58f33ba8dc2249a5ea595d018b238e5724a
Timo Partti <timo.partti@columbiaroad.com>
2021-08-18 12:02:49 +0300

    init

---
+++ Set6.hs
@@ -0,0 +1,270 @@
+-- Exercise set 6: defining classes and instances
+
+
+module Set6 where
+
+import Mooc.Todo
+import Data.Char (toLower)
+
+------------------------------------------------------------------------------
+-- Ex 1: define an Eq instance for the type Country below. You'll need
+-- to use pattern matching.
+
+data Country = Finland | Switzerland | Norway
+  deriving Show
+
+instance Eq Country where
+  Finland == Finland = True
+  Switzerland == Switzerland = True
+  Norway == Norway = True
+  _ == _ = False
+
+------------------------------------------------------------------------------
+-- Ex 2: implement an Ord instance for Country so that
+--   Finland <= Norway <= Switzerland
+--
+-- Remember minimal complete definitions!
+
+instance Ord Country where
+  compare x y = compare (numbered x) (numbered y) where
+      numbered Finland = 1
+      numbered Norway = 2
+      numbered Switzerland = 3
+
+  -- (<=) = todo -- and me?
+  -- min = todo -- and me?
+  -- max = todo -- and me?
+
+------------------------------------------------------------------------------
+-- Ex 3: Implement an Eq instance for the type Name which contains a String.
+-- The Eq instance should ignore capitalization.
+--
+-- Hint: use the function Data.Char.toLower that has been imported for you.
+--
+-- Examples:
+--   Name "Pekka" == Name "pekka"   ==> True
+--   Name "Pekka!" == Name "pekka"  ==> False
+
+data Name = Name String
+  deriving Show
+
+instance Eq Name where
+  (==) (Name x) (Name y) = map toLower x == map toLower y
+
+------------------------------------------------------------------------------
+-- Ex 4: here is a list type parameterized over the type it contains.
+-- Implement an instance "Eq (List a)" that compares the lists element
+-- by element.
+--
+-- Note how the instance needs an Eq a constraint. What happens if you
+-- remove it?
+
+data List a = Empty | LNode a (List a)
+  deriving Show
+
+instance Eq a => Eq (List a) where
+  (==) (Empty) (Empty) = True
+  (==) Empty _ = False
+  (==) _ Empty = False
+  (==) (LNode x xs) (LNode y ys) = if x == y then (==) xs ys else False
+
+------------------------------------------------------------------------------
+-- Ex 5: below you'll find two datatypes, Egg and Milk. Implement a
+-- type class Price, containing a function price. The price function
+-- should return an Int representing the price of the item in a store.
+--
+-- The prices should be as follows:
+-- * chicken eggs cost 20
+-- * chocolate eggs cost 30
+-- * milk costs 15 per liter
+
+data Egg = ChickenEgg | ChocolateEgg
+  deriving Show
+data Milk = Milk Int -- amount in litres
+  deriving Show
+
+class Price a where
+  price :: a -> Int
+
+instance Price Egg where
+  price ChickenEgg = 20
+  price ChocolateEgg = 30
+
+instance Price Milk where
+  price (Milk x) = 15 * x
+
+instance Price a => Price (Maybe a) where
+  price (Just x) = price x
+  price Nothing = 0
+
+instance Price a => Price [a] where
+  price [] = 0
+  price (x:xs) = price x + price xs
+
+------------------------------------------------------------------------------
+-- Ex 6: define the necessary instances in order to be able to compute these:
+--
+-- price [Just (ChocolateEgg), Nothing, Just (ChickenEgg)] ==> 50
+-- price [Nothing, Nothing, Just (Milk 1), Just (Milk 2)]  ==> 45
+
+
+------------------------------------------------------------------------------
+-- Ex 7: below you'll find the datatype Number, which is either an
+-- Integer, or a special value Infinite.
+--
+-- Implement an Ord instance so that finite Numbers compare normally,
+-- and Infinite is greater than any other value.
+
+data Number = Finite Integer | Infinite
+  deriving (Show,Eq)
+
+instance Ord Number where
+  compare Infinite Infinite = EQ
+  compare Infinite _ = GT
+  compare _ Infinite = LT
+  compare (Finite x) (Finite y) = compare x y
+
+------------------------------------------------------------------------------
+-- Ex 8: rational numbers have a numerator and a denominator that are
+-- integers, usually separated by a horizontal bar or a slash:
+--
+--      numerator
+--    -------------  ==  numerator / denominator
+--     denominator
+--
+-- You may remember from school that two rationals a/b and c/d are
+-- equal when a*d == b*c. Implement the Eq instance for rationals
+-- using this definition.
+--
+-- You may assume in all exercises that the denominator is always
+-- positive and nonzero.
+--
+-- Examples:
+--   RationalNumber 4 5 == RationalNumber 4 5    ==> True
+--   RationalNumber 12 15 == RationalNumber 4 5  ==> True
+--   RationalNumber 13 15 == RationalNumber 4 5  ==> False
+
+data RationalNumber = RationalNumber Integer Integer
+  deriving Show
+
+instance Eq RationalNumber where
+  (RationalNumber p p2) == (RationalNumber q q2) = p * q2 == p2 * q
+
+------------------------------------------------------------------------------
+-- Ex 9: implement the function simplify, which simplifies rational a
+-- number by removing common factors of the numerator and denominator.
+-- In other words,
+--
+--     ca         a
+--    ----  ==>  ---
+--     cb         b
+--
+-- As a concrete example,
+--
+--     12        3 * 4         4
+--    ----  ==  -------  ==>  ---.
+--     15        3 * 5         5
+--
+-- Hint: Remember the function gcd?
+
+simplify :: RationalNumber -> RationalNumber
+simplify (RationalNumber p q) = RationalNumber (p `div` nom) (q `div` nom)
+  where nom = gcd p q
+
+------------------------------------------------------------------------------
+-- Ex 10: implement the typeclass Num for RationalNumber. The results
+-- of addition and multiplication must be simplified.
+--
+-- Reminders:
+--   * negate x is 0-x
+--   * abs is absolute value
+--   * signum is -1, +1 or 0 depending on the sign of the input
+--
+-- Examples:
+--   RationalNumber 1 3 + RationalNumber 1 6 ==> RationalNumber 1 2
+--   RationalNumber 1 3 * RationalNumber 3 1 ==> RationalNumber 1 1
+--   negate (RationalNumber 2 3)             ==> RationalNumber (-2) 3
+--   fromInteger 17 :: RationalNumber        ==> RationalNumber 17 1
+--   abs (RationalNumber (-3) 2)             ==> RationalNumber 3 2
+--   signum (RationalNumber (-3) 2)          ==> RationalNumber (-1) 1
+--   signum (RationalNumber 0 2)             ==> RationalNumber 0 1
+
+instance Num RationalNumber where
+  (RationalNumber p q) + (RationalNumber p2 q2) = simplify (RationalNumber (p * q2 + q * p2) (q * q2))
+  (RationalNumber p q) * (RationalNumber p2 q2) = simplify (RationalNumber (p * p2) (q * q2))
+  abs (RationalNumber p q) = RationalNumber (abs p) (abs q)
+  signum (RationalNumber p q) = RationalNumber (signum p) (signum q)
+  fromInteger x = RationalNumber x 1
+  negate (RationalNumber p q) = RationalNumber (-p) q
+
+------------------------------------------------------------------------------
+-- Ex 11: a class for adding things. Define a class Addable with a
+-- constant `zero` and a function `add`. Define instances of Addable
+-- for Integers and lists. Numbers are added with the usual addition,
+-- while lists are added by catenating them. Pick a value for `zero`
+-- such that: `add zero x == x`
+--
+-- Examples:
+--   add 1 2                ==>  3
+--   add 1 zero             ==>  1
+--   add [1,2] [3,4]        ==>  [1,2,3,4]
+--   add zero [True,False]  ==>  [True,False]
+
+class Addable a where
+  zero :: a
+  add :: a -> a -> a
+
+instance Addable Integer where
+  zero = 0
+  add a b = a + b
+
+instance Addable Bool where
+  zero = False
+  add a b = b
+
+instance Addable a => Addable [a] where
+  zero = []
+  add a b = a ++ b
+
+------------------------------------------------------------------------------
+-- Ex 12: cycling. Implement a type class Cycle that contains a
+-- function `step` that cycles through the values of the type.
+-- Implement instances for Color and Suit that work like this:
+--
+--   step Red ==> Green
+--   step Green ==> Blue
+--   step Blue ==> Red
+--
+-- The suit instance should cycle suits in the order Club, Spade,
+-- Diamond, Heart, Club.
+--
+-- Also add a function `stepMany` and give it a default implementation
+-- using `step`. The function `stepMany` should take multiple
+-- (determined by an Int argument) steps like this:
+--
+--   stepMany 2 Club ==> Diamond
+--   stepMany 3 Diamond ==> Spade
+
+data Color = Red | Green | Blue
+  deriving (Show, Eq)
+data Suit = Club | Spade | Diamond | Heart
+  deriving (Show, Eq)
+
+class Cycle a where
+  step :: a -> a
+  
+  stepMany :: Int -> a -> a
+  stepMany 0 c = c
+  stepMany x c = stepMany (x - 1) (step c)
+
+instance Cycle Color where
+  step Red = Green
+  step Green = Blue
+  step Blue = Red
+
+instance Cycle Suit where
+  step Club = Spade
+  step Spade = Diamond
+  step Diamond = Heart
+  step Heart = Club
+
